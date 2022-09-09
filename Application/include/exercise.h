392952963c8ee59bdda36d2abe3d5d81171e43c7
Oleg Loshkin <loshkin.oleg.95@gmail.com>
2022-09-09 13:10:42 +0200

    Fixed wrong profiling color being used for Async in exercise.h

--- Application/include/exercise.h
+++ Application/include/exercise.h
@@ -29,12 +29,12 @@ float SingleThread(const size_t iterations)
 // Approximates PI by kicking off smaller pi approximating subroutines but lets them instanciate their own random number generators.
 float Async(const size_t iterations, const size_t nrOfWorkers)
 {
-	EASY_BLOCK("Async method.", profiler::colors::Blue);
+	EASY_BLOCK("Async method.", profiler::colors::Red);
 
 	// Implementation of the PI approximating algorithm but this time split into multiple subroutines with their own random number generators.
 	const auto approximatePi = [](const size_t iterations, const size_t nrOfWorkers, const size_t workerId)->size_t
 	{
-		EASY_BLOCK("Approximation subroutine.", profiler::colors::Blue100);
+		EASY_BLOCK("Approximation subroutine.", profiler::colors::Red100);
 
 		/*TODO:
 			Generate 2D points within a unit square using e, d, iterationsand nrOfWorkersand count the number of points that lies inside the unit circle.
@@ -48,7 +48,7 @@ float Async(const size_t iterations, const size_t nrOfWorkers)
 
 	std::vector<std::future<size_t>> futures(nrOfWorkers);
 	{
-		EASY_BLOCK("Kicking off threads.", profiler::colors::Blue);
+		EASY_BLOCK("Kicking off threads.", profiler::colors::Red100);
 		
 		/*TODO:
 			Kick off std::async's executing approximatePi.
@@ -57,7 +57,7 @@ float Async(const size_t iterations, const size_t nrOfWorkers)
 
 	size_t insideCircle = 0;
 	{
-		EASY_BLOCK("Retrieving results.", profiler::colors::Blue);
+		EASY_BLOCK("Retrieving results.", profiler::colors::Red100);
 		
 		/*TODO:
 			Retrieve the results of the subroutines computed asynchnously.
@@ -70,12 +70,12 @@ float Async(const size_t iterations, const size_t nrOfWorkers)
 // Approximates PI by kicking off smaller pi approximating subroutines guaranteed to be on different threads and lets them instanciate their own random number generators.
 float Threads(const size_t iterations, const size_t nrOfWorkers)
 {
-	EASY_BLOCK("Threads method.", profiler::colors::Yellow);
+	EASY_BLOCK("Threads method.", profiler::colors::Blue);
 
 	// Modified version of approximatePi that uses a std::promise to return the result instead of the return value of the function.
 	const auto approximatePi = [](/* TODO: Modify the signature of this lambda so that it uses a std::promise to return a value instead of the regular return value. */)
 	{
-		EASY_BLOCK("Approximation subroutine.", profiler::colors::Yellow100);
+		EASY_BLOCK("Approximation subroutine.", profiler::colors::Blue100);
 		
 		/*TODO:
 			Implement an approximatePi like the one from AsyncNoRef but this time use a std::promise to return the result.
@@ -85,7 +85,7 @@ float Threads(const size_t iterations, const size_t nrOfWorkers)
 	std::vector<std::thread> threads; // Vector for all the threads we'll be kicking off.
 	std::vector<std::future<size_t>> futures; // And a vector for holding their associated futures to retireve their results.
 	{
-		EASY_BLOCK("Kicking off threads.", profiler::colors::Yellow);
+		EASY_BLOCK("Kicking off threads.", profiler::colors::Blue100);
 		
 		/*TODO:
 			Kick off std::async's executing approximatePi.
@@ -94,7 +94,7 @@ float Threads(const size_t iterations, const size_t nrOfWorkers)
 
 	size_t insideCircle = 0;
 	{
-		EASY_BLOCK("Retrieving results.", profiler::colors::Yellow);
+		EASY_BLOCK("Retrieving results.", profiler::colors::Blue100);
 		
 		/*TODO:
 			Retrieve the results of the subroutines computed asynchnously.
